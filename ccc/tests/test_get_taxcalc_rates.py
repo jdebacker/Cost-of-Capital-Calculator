eff0827d4b89ae2354c53b03f8c1f2fe288e5f08
jdebacker <jason.debacker@gmail.com>
2020-11-08 16:25:52 -0500

    update tests of get tax calc rates

--- ccc/tests/test_get_taxcalc_rates.py
+++ ccc/tests/test_get_taxcalc_rates.py
@@ -9,7 +9,7 @@ def test_get_calculator_cps():
     '''
     Test the get_calculator() function
     '''
-    calc1 = tc.get_calculator(True, 2019)
+    calc1 = tc.get_calculator(True, 2019, 2019)
     assert calc1.current_year == 2019
 
 
@@ -20,7 +20,7 @@ def test_get_calculator(data):
     '''
     Test the get_calculator() function
     '''
-    calc1 = tc.get_calculator(True, 2019, data=data)
+    calc1 = tc.get_calculator(True, 2019, 2019, data=data)
     assert calc1.current_year == 2019
 
 
@@ -36,9 +36,11 @@ def test_get_rates():
     '''
     Test of the get_rates() functions
     '''
-    p = Specification(year=2020)  # has default tax rates, with should equal TC
+    # has default tax rates, which should equal what is returned from TC
+    p = Specification(start_year=2020, end_year=2020)
     test_dict = tc.get_rates(
-        baseline=False, start_year=2020, reform={}, data='cps')
+        baseline=False, start_year=2020, end_year=2020, reform={},
+        data='cps')
     for k, v in test_dict.items():
         print('Tax rate = ', k)
         assert(np.allclose(v, p.__dict__[k]))
